dcf187cf14cbb6d70283d07dd26f34442d5b4937
saul rodriguez <editor.taisei@gmail.com>
2026-01-23 09:21:26 -0500

    feat(formatoAnalisis): add exception handling

---
+++ backend/analisis_clinico/src/main/java/com/laboratorio/analisis_clinico/formatoAnalisis/application/exception/FormatoNotFoundException.java
@@ -0,0 +1,7 @@
+package com.laboratorio.analisis_clinico.formatoAnalisis.application.exception;
+
+public class FormatoNotFoundException extends RuntimeException{
+    public FormatoNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ backend/analisis_clinico/src/main/java/com/laboratorio/analisis_clinico/formatoAnalisis/domain/exception/FormatoDomainException.java
@@ -0,0 +1,7 @@
+package com.laboratorio.analisis_clinico.formatoAnalisis.domain.exception;
+
+public class FormatoDomainException extends RuntimeException{
+    public FormatoDomainException(String message) {
+        super(message);
+    }
+}
